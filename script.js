395c41ad5f3a7eb3d1e53837179693f4d3d95ce6
Figby222 <figby222@outlook.com>
2024-02-11 05:17:06 -0800

    Modify: script.js
    Change turns after each round

--- script.js
+++ script.js
@@ -2,34 +2,46 @@ const Gameboard = (function () {
     let board = [];
     const rows = 3;
     const columns = 3;
+    let currentTurn;
 
     for (let row = 0; row < rows; row++) {
         board[row] = [];
         for (let column = 0; column < columns; column++) {
-            board[row].push (Cell());
+            board[row].push(new Cell());
         }
     }
     
     const getBoard = () => board;
+
+    const addMarker = (x, y) => {
+        board[x][y] = currentTurn.marker;
+    }
     
-    
+    const getTurn = () => currentTurn;
+
+    const changeTurn = (player) => {
+        currentTurn = player;
+    }
 
     return {
         getBoard,
+        addMarker,
+        getTurn,
+        changeTurn,
     }
 })();
 
 function Cell() {
-    let marker;
+    this.marker;
 
-    const markCell = (player) => marker = player;
+    const setMark = (player) => this.marker = player;
 
     const getMark = () => marker;
 
-    return {
-        markCell,
-        getMark,
-    }
+    // return {
+    //     setMark,
+    //     getMark
+    // }
 }
 
 function Player(name, marker) {
@@ -40,18 +52,32 @@ function Player(name, marker) {
 function playRound(player) {
     let currentCell = prompt("Choose a cell to mark");
     currentCell = currentCell.split(" ");
-    while ((currentCell[0] > 2 || currentCell[0] < 0) || (currentCell[1] > 2 || currentCell[1] < 0)) {
+
+    while ((currentCell[0] > 2 || currentCell[0] < 0) || (currentCell[1] > 2 || currentCell[1] < 0) || isNaN(currentCell[0]) || isNaN(currentCell[1])) {
         currentCell = prompt("Choose a proper cell");
         currentCell.split(" ");
     }
-    Gameboard.markCell(player, currentCell[0], currentCell[1]);
+
+    Gameboard.addMarker(currentCell[0], currentCell[1]);
+    console.table(Gameboard.getBoard());
+    
 }
 
 const Controller = (function () {
     const playerX = new Player("Player One", "X");
     const playerO = new Player("Player Two", "O");
+    
+    Gameboard.changeTurn(playerX);
 
-    playRound(playerX);
+    while (true)
+        {playRound(Gameboard.currentTurn);
+
+        if (Gameboard.getTurn() == playerX) {
+            Gameboard.changeTurn(playerO);
+        } else {
+            Gameboard.changeTurn(playerX);
+        }
+    }
 
     return {
         playerX,
